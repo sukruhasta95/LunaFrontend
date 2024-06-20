e8e6b9570090d50075c1aa39cbfa381d6a438e3e
şükrü hasta <sukruhasta95@gmail.com>
2024-06-20 22:19:53 +0300

    create-components-and-install-mui

--- LunaFrontend/src/app/app.routes.ts
+++ LunaFrontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CommonComponent } from '../Components/Common/common.component';
+import { MeterComponent } from '../Components/Meter/meter.component';
+import { ReportComponent } from '../Components/Report/report.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CommonComponent },
+  { path: 'meter', component: MeterComponent },
+  { path: 'report', component: ReportComponent }
+];
